e434f90a59a13b2c6132cc0befd65ba54336493b
Yerlan <eneszhanov@kassa24.kz>
2019-11-01 16:21:16 +0600

    ADDED: sign page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shop/shop";
 import Header from "./components/header/header";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
 function App() {
   return (
     <div>
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
